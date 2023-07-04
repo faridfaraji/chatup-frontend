a385745d7f1e95b82ce0326bc0fee71a3f01038c
RayArenar <rasoul1377@gmail.com>
2023-07-04 11:27:25 -0700

    misc changes

--- extensions/chat-bot/assets/chat_bot.js
+++ extensions/chat-bot/assets/chat_bot.js
@@ -12,7 +12,7 @@ function hideLoader() {
 }
 
 // Define regular expressions to match URLs, emails, and phone numbers
-const urlRegex = /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig;
+const urlRegex = /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|](\.[A-Z]{2,9}\b)([-A-Z0-9+&@#\/%=~_|](\.[A-Z]{2,9}\b))*)(?![^\s])(?=\W|$)/ig;
 const emailRegex = /(\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}\b)/ig;
 const phoneRegex = /(\b(?:\+?1\s*\(?[2-9][0-8][0-9]\)?\s*|0?[2-9][0-8][0-9]\s*)(?:[.-]\s*)?(?:[2-9][0-9]{2}\s*)(?:[.-]\s*)?[0-9]{4}\b)/ig;
 
