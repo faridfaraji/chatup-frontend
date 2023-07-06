3fbc9945a6b05a3db1e5f0193043a3620f72d044
Sam Churchill <meadowschurchill@gmail.com>
2023-07-05 22:31:08 -0700

    Didnt' define payload

--- hooks/useTemperature.jsx
+++ hooks/useTemperature.jsx
@@ -5,7 +5,7 @@ export const useTemperature = () => {
     const fetch = useAuthenticatedFetch();
     const fetch_url = `${constants.gateway_url}/database/${constants.app_name}/bot-temperature`
     return (bot_temperature) => {
-        payload = {bot_temperature: parseFloat(bot_temperature)}
+        const payload = {bot_temperature: parseFloat(bot_temperature)}
         return fetch(fetch_url, {
             method: "PUT",
             credentials: constants.credentials,
