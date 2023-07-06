f59c29fa97ff054d2cf5fa17939c6bf1e4777050
Sam Churchill <meadowschurchill@gmail.com>
2023-07-06 11:10:54 -0700

    Update hook to accept a date range

--- hooks/useChatHistory.jsx
+++ hooks/useChatHistory.jsx
@@ -3,20 +3,21 @@ import { useAuthenticatedFetch } from ".";
 
 export const useChatHistory = () => {
     const fetch = useAuthenticatedFetch();
-    return async () => {
-        try {
-            const fetch_url = `${constants.gateway_url}/database/${constants.app_name}/conversations`
-            const response = await fetch(fetch_url, {
-                method: 'GET',
-                credentials: constants.credentials,
-                headers: constants.headers
-            })
-            if (response.ok) {
-                const data = await response.json()
-                return data
-            }
-        } catch (error) {
-            console.error('Chat History error:', error)
+    return (startDateTime, endDateTime) => {
+        const fetch_url = `${constants.gateway_url}/database/${constants.app_name}/conversations`
+        const queryParams = new URLSearchParams();
+
+        if (startDateTime && endDateTime) {
+            queryParams.append("start_datetime", startDateTime.toISOString().slice(0, 19).replace("T", " "));
+            queryParams.append("end_datetime", endDateTime.toISOString().slice(0, 19).replace("T", " "));
         }
+
+        const urlWithParams = queryParams.toString() ? `${fetch_url}?${queryParams.toString()}` : fetch_url;
+
+        return fetch(urlWithParams, {
+            method: 'GET',
+            credentials: constants.credentials,
+            headers: constants.headers
+        }).then((response) => response.json())
     }
 }
