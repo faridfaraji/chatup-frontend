a70a4cbc6c7ea86583aedf883ab9fe96b926fbda
Sam Churchill <meadowschurchill@gmail.com>
2023-07-08 20:04:26 -0700

    Translated personality traits

--- components/Temperature.jsx
+++ components/Temperature.jsx
@@ -3,9 +3,11 @@ import { useState, useCallback, useEffect } from 'react';
 import { useTemperature } from '../hooks/useTemperature';
 import { useShop } from '../hooks';
 import { tempString } from '../utils/dataUtils';
+import { useTranslation } from 'react-i18next';
 
 
 export const Temperature = (props) => {
+    const {t} = useTranslation();
     const [selected, setSelected] = useState("");
     const sendTemperature = useTemperature();
     const getShop = useShop();
@@ -30,11 +32,11 @@ export const Temperature = (props) => {
 
     const options = [
         { label: "", value: null},
-        { label: "Professional", value: "0.00" },
-        { label: "Friendly", value: "0.25" },
-        { label: "Informal", value: "050" },
-        { label: "Engaging", value: "0.75" },
-        { label: "Humorous", value: "1.00" },
+        { label: t("Configuration.professional"), value: "0.00" },
+        { label: t("Configuration.friendly"), value: "0.25" },
+        { label: t("Configuration.informal"), value: "050" },
+        { label: t("Configuration.engaging"), value: "0.75" },
+        { label: t("Configuration.humorous"), value: "1.00" },
     ]
 
     return (
