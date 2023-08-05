1e9c10353acba4b7903bf2042c068d0e2fffb5c2
Sam Churchill <meadowschurchill@gmail.com>
2023-08-05 12:09:12 -0700

    remove test button from live app

--- pages/index.jsx
+++ pages/index.jsx
@@ -117,7 +117,7 @@ export default function HomePage() {
       <SkeletonHomePage /> :
       <Page
         title="Welcome to ChatUp"
-        primaryAction={testButton}
+{/*         primaryAction={testButton} */}
       >
         <Layout>
           <Layout.Section fullWidth>
