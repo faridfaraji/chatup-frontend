66d47785be44319f12b6a1b10692fa57d0a883ea
Sam Churchill <meadowschurchill@gmail.com>
2023-11-07 10:23:07 -0800

    Remove validation

--- extensions/chat-bot/assets/main.js
+++ extensions/chat-bot/assets/main.js
@@ -2,7 +2,7 @@ import { addEventListeners } from "./events.js";
 import { validate } from "./io.js";
 import { establishSocket, listenForMessages } from "./socket.js";
 
-validate();
+// validate();
 const socket = establishSocket();
 addEventListeners(socket);
 listenForMessages(socket);
